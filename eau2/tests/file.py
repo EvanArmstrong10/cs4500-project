910c4d7f33d20ffbf0694a0c16dfccf3f3255985
Evan Armstrong <evanarmstrongcse@gmail.com>
2020-03-13 19:50:05 -0400

    added data generation script

--- eau2/tests/file.py
+++ eau2/tests/file.py
@@ -5,14 +5,20 @@ import random
 f = open("datafile.txt", "w+")
 
 
-ints = [1, 9, 34, 56, 24, 37, 92, 45, 54, 20, 34, 28, 10, 32, 72, 123]
+ints = ['<1>', '<9>', '<34>', '<56>', '<24>', '<37>', '<92>', '<45>', '<54>', '<20>', '<34>', '<28>', '<10>', '<32>', '<72>', '<123>']
 
 line = ""
-for i in range(0, 100):
-    line += str(ints[random.randint(0, 15)])
-    line += " "
+lines = []
 
-for i in range(0, 1000000):
-    f.write(line + "\r")
+for x in range(0, 10):
+
+    for i in range(0, 10):
+        line += ints[random.randint(0, 15)]
+        line += " "
+    lines.append(line)
+    line = ""
+
+for i in range(0, 10):
+    f.write(lines[random.randint(0, 9)] + "\r")
 
 f.close()
